f60956cf4f394cb76fbb51a06b5e145733aa6830
Nishant <rd5762331@g,ail.com>
2025-04-24 10:37:00 +0530

    Add

--- backend/controllers/webhooks.js
+++ backend/controllers/webhooks.js
@@ -1,11 +1,11 @@
 import { Webhook } from "svix";
 import User from "../models/User.js";
 
-const clerkWeebhooks = async(req,red) =>{
+const clerkWeebhooks = async(req,res) =>{
     try{
 const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
 
-await whook.verify(json.stringify(req.body),{
+await whook.verify(JSON.stringify(req.body),{
 "svix-id": req.headers["svix-id"],
 "svix-timestamp":req.headers["svix-timestamp"],
 "svix-signature": req.headers["svix-signature"]
@@ -15,8 +15,8 @@ switch(type){
     case 'user.created':{
  const userData  = {
     _id: data.id,
-    email:data.email_address[0].email_address,
-    name: data.first_name + " " +last_name,
+    email: data.email_address && data.email_address[0] ? data.email_address[0].email_address : '',
+    name: data.first_name + " " +data.last_name,
     imageUrl: data.image_url,
  }
  await User.create(userData)
@@ -26,7 +26,7 @@ switch(type){
     case'user.updated':{
         const userData  = {
             email:data.email_address[0].email_address,
-            name: data.first_name + " " +last_name,
+            name: data.first_name + " " +data.last_name,
             imageUrl: data.image_url,
          }
          await User.findByIdAndUpdate(data.id, userData)
